990bad60f96b1f6bb62dd4ed3b64385f904849a3
Christian Scheb <info@stylemotion.de>
2014-01-19 13:22:02 +0100

    Added annotations

--- ApiClient.php
+++ ApiClient.php
@@ -9,9 +9,12 @@ class ApiClient
 
     /**
      * Get historical data for a symbol
+     *
      * @param string $symbol
      * @param \DateTime $startDate
      * @param \DateTime $endDate
+     * @return array
+     * @throws \Scheb\YahooFinanceApi\Exception\ApiException
      */
     public function getHistoricalData($symbol, \DateTime $startDate, \DateTime $endDate)
     {
@@ -23,7 +26,10 @@ class ApiClient
 
     /**
      * Get quotes for one or multiple symbols
+     *
      * @param array|string $symbols
+     * @return array
+     * @throws \Scheb\YahooFinanceApi\Exception\ApiException
      */
     public function getQuotes($symbols)
     {
@@ -39,7 +45,10 @@ class ApiClient
 
     /**
      * Get quotes list for one or multiple symbols
+     *
      * @param array|string $symbols
+     * @return array
+     * @throws \Scheb\YahooFinanceApi\Exception\ApiException
      */
     public function getQuotesList($symbols)
     {
