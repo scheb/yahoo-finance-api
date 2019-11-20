79c754cc97691827574a36a6fa3ba3e4c422e83f
AbramovVyacheslav <abramovvyacheslav@gmail.com>
2019-11-20 15:33:54 +0300

    Update ApiClient.php

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -152,10 +152,7 @@ class ApiClient
      */
     private function fetchQuotes(array $symbols)
     {
-//        $url = 'https://query1.finance.yahoo.com/v7/finance/quote?symbols='.urlencode(implode(',', $symbols));
-        $url = 'https://query1.finance.yahoo.com/v7/finance/quote?symbols='.implode(',', $symbols);
-
-//        dd($url);
+        $url = 'https://query1.finance.yahoo.com/v7/finance/quote?symbols='.urlencode(implode(',', $symbols));
 
         $responseBody = (string) $this->client->request('GET', $url)->getBody();
 
