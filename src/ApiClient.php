8365dfe03bad8fb08205a2776f4403906c80a7f2
Christian Scheb <privat@stylemotion.de>
2023-05-07 13:36:25 +0200

    Switch quote data to v6 endpoint, since v7 stopped working (Unauthorized / Invalid cookie / Invalid crumb)
    Fixes #38
    Fixes #39

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -190,7 +190,7 @@ class ApiClient
      */
     private function fetchQuotes(array $symbols): array
     {
-        $url = 'https://query1.finance.yahoo.com/v7/finance/quote?symbols='.urlencode(implode(',', $symbols));
+        $url = 'https://query1.finance.yahoo.com/v6/finance/quote?symbols='.urlencode(implode(',', $symbols));
         $responseBody = (string) $this->client->request('GET', $url)->getBody();
 
         return $this->resultDecoder->transformQuotes($responseBody);
