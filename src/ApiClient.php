7c145d92f1ed7e70094e87ca50bbc555b380605b
Elminson De Oleo Baez <edeoleo@gmail.com>
2024-03-01 13:20:45 -0500

    moving var for readability

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -257,7 +257,6 @@ class ApiClient
 
         // Initialize session cookies
         $initialUrl = 'https://fc.yahoo.com';
-        $modules = 'financialData,quoteType,defaultKeyStatistics,assetProfile,summaryDetail';
         $this->client->request('GET', $initialUrl, ['cookies' => $cookieJar, 'http_errors' => false, 'headers' => $this->getHeaders()]);
 
         // Get crumb value
@@ -265,6 +264,7 @@ class ApiClient
         $crumb = (string) $this->client->request('GET', $initialUrl, ['cookies' => $cookieJar, 'headers' => $this->getHeaders()])->getBody();
 
         // Fetch quotes
+		$modules = 'financialData,quoteType,defaultKeyStatistics,assetProfile,summaryDetail';
         $url = 'https://query'.$qs.'.finance.yahoo.com/v10/finance/quoteSummary/'.$symbol.'?crumb='.$crumb.'&modules='.$modules;
         $responseBody = (string) $this->client->request('GET', $url, ['cookies' => $cookieJar, 'headers' => $this->getHeaders()])->getBody();
 
