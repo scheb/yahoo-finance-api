a0513e9fd78c589fb44231959bcd2f00c063437a
Jose Rodrigues <jrodriguesd@gmail.com>
2024-10-16 09:48:27 +0100

    Solve No Data Issue

--- src/ResultDecoder.php
+++ src/ResultDecoder.php
@@ -202,7 +202,12 @@ class ResultDecoder
         }
 
         $result = $decoded['chart']['result'][0];
-        $entryCount = \count($result['timestamp']);
+
+        if (0 === \count($result['indicators']['quote'][0])) {
+            return [];
+        }
+
+        $entryCount = \count($result['indicators']['quote'][0]['open']);
         $returnArray = [];
         for ($i = 0; $i < $entryCount; ++$i) {
             $returnArray[] = $this->createHistoricalData($result, $i);
@@ -249,6 +254,11 @@ class ResultDecoder
             throw new ApiException('Response is not a valid JSON', ApiException::INVALID_RESPONSE);
         }
 
+        if (!isset($decoded['chart']['result'][0]['events']['dividends'])) {
+            return [];
+            // throw new ApiException('Response is not a valid JSON', ApiException::INVALID_RESPONSE);
+        }
+
         return array_map(function (array $item) {
             return $this->createDividendData($item);
         }, $decoded['chart']['result'][0]['events']['dividends']);
@@ -273,6 +283,11 @@ class ResultDecoder
         $decoded = json_decode($responseBody, true);
         if ((!\is_array($decoded)) || (isset($decoded['chart']['error']))) {
             throw new ApiException('Response is not a valid JSON', ApiException::INVALID_RESPONSE);
+        }
+
+        if (!isset($decoded['chart']['result'][0]['events']['splits'])) {
+            return [];
+            // throw new ApiException('Response is not a valid JSON', ApiException::INVALID_RESPONSE);
         }
 
         return array_map(function (array $item) {
