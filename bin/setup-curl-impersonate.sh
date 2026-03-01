8214426b978920795e13c3d98808bab3a96da6b8
Christian Scheb <privat@stylemotion.de>
2026-03-01 12:04:01 +0100

    Update curl-impersonate

--- bin/setup-curl-impersonate.sh
+++ bin/setup-curl-impersonate.sh
@@ -11,8 +11,8 @@ fi
 mkdir -p "$INSTALL_DIR" || true
 cd "$INSTALL_DIR"
 
-wget -O libcurl-impersonate.tar.gz https://github.com/lexiforest/curl-impersonate/releases/download/v1.0.3/libcurl-impersonate-v1.0.3.x86_64-linux-gnu.tar.gz
-echo "e4d2066f7f1c544a2a0ddadfe1d164cb3daffdefcb3143363b0afd6d2f2125e7  libcurl-impersonate.tar.gz" > checksum.sha256
+wget -O libcurl-impersonate.tar.gz https://github.com/lexiforest/curl-impersonate/releases/download/v1.3.1/libcurl-impersonate-v1.3.1.x86_64-linux-gnu.tar.gz
+echo "14166d04cac7fb241c041a0f847de96ccca77e7eda483edf73d65f906e1d38e6  libcurl-impersonate.tar.gz" > checksum.sha256
 sha256sum --check checksum.sha256 || exit 1
 tar -xf libcurl-impersonate.tar.gz
 
